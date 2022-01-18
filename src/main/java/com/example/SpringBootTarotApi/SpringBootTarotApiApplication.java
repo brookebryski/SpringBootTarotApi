e5d254c7d3dc55d4bfe285236b761de75e0b4e10
BrookeBryski <brookeabryski@gmail.com>
2022-01-18 17:35:28 -0600

    initial commit

---
+++ src/main/java/com/example/SpringBootTarotApi/SpringBootTarotApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootTarotApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTarotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTarotApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBootTarotApi/SpringBootTarotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootTarotApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTarotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
