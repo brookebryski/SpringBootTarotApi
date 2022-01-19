36c7377fdf2cf8c166e5bf28fd62ca32300dd038
BrookeBryski <brookeabryski@gmail.com>
2022-01-19 11:34:41 -0600

    tarot controller and finished up app

--- src/main/java/com/example/SpringBootTarotApi/Controller/TarotController.java
+++ src/main/java/com/example/SpringBootTarotApi/Controller/TarotController.java
@@ -1,4 +1,24 @@
 package com.example.SpringBootTarotApi.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
 public class TarotController {
+    @Autowired
+    Tarot tarot;
+
+    AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/tarot")
+    public Tarot tarot(@RequestParam(value="card")String card)
+    {
+        tarot.setId(counter.incrementAndGet());
+        tarot.setContent("The card you chose is " +card);
+        return tarot;
+    }
 }
--- src/main/java/com/example/SpringBootTarotApi/SpringBootTarotApiApplication.java
+++ src/main/java/com/example/SpringBootTarotApi/SpringBootTarotApiApplication.java
@@ -11,3 +11,6 @@ public class SpringBootTarotApiApplication {
 	}
 
 }
+
+// http://localhost:8080/tarot?card=Empress
+// http://localhost:8080/tarot?card=hanged%20man
